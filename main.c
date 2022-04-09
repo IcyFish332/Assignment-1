1640e3cf917cb662aaba58c74d8844c1dea968fe
kevinwang66 <571390141@qq.com>
2022-04-09 18:55:30 +0800

    kevin is here

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("Hope we can enjoy this trip!\n")
+    printf("Hope we can enjoy this trip!\n");
     return 0;
+    //kevin is here
     //我想改一下
 }
