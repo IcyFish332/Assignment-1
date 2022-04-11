270b299457a24b6ab45646dc811d1f346615b0a7
Siyuan <lusiyuanzs@gmail.com>
2022-04-11 20:39:37 +0800

    add one .h file,bulid main flie

---
+++ base.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int TOTAL_STATION =0;
+char * STRATEGY = NULL;
+int DISTANCE=0;
+int pos=0;//short for position
+int tar=0;//short for target
+int wise=0;//short for clockwise
+int ccwise-0;//short for counterclockwise
+
+void FCFS();
+void SSTF();
+void SCAN();
--- main.c
+++ main.c
@@ -1,14 +1,34 @@
-#include <stdio.h>
+#include "base.h"
 
-int main() {
-    printf("Hello, World!\n");
-    printf("Hope we can enjoy this trip!\n");
-    //可乐吗
-    return 0;
-    //kevin is here
-
-    //我想改一下
-
-    //test12312
-    printf("we are here");
+int main()
+{
+    FILE *p=fopen("dict.dic","r");
+    char buffer[1024];
+    char * token;
+    while(p!=EOF){
+        fscanf(p,"%s",buffer);
+        if(buffer[0]!='#'){
+            token=strtok(buffer," ");
+            if(token=="TOTAL_STATION"){
+                token=strtok(NULL," ");
+                token=strtok(NULL," ");
+                TOTAL_STATION=atoi(token);
+            }
+            else if(token=="DISTANCE"){
+                token=strtok(NULL," ");
+                token=strtok(NULL," ");
+                DISTANCE=atoi(token);
+            }
+            else if(token=="STRATEGY"){
+                token=strtok(NULL," ");
+                token=strtok(NULL," ");
+                STRATEGY=token;
+            }
+        }
+    }
+    fclose(p);
+    if(TOTAL_STATION==0) TOTAL_STATION=5;
+    if(DISTANCE==0) DISTANCE=2;
+    if(STRATEGY==NULL) STRATEGY="FCFS";
+    printf("%d %d %s\n",TOTAL_STATION,DISTANCE,STRATEGY);
 }
