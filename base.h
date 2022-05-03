b03715d2eb087b510757f96dacb37a49db8e3b32
Siyuan <lusiyuanzs@gmail.com>
2022-05-03 17:05:09 +0800

    small change

--- base.h
+++ base.h
@@ -12,11 +12,6 @@ extern int direction;//0->cwise,1->ccwise
 extern int pos;//short for position
 extern string order;
 
-void config();
-void FCFS();
-void SSTF();
-void SCAN();
-
 typedef struct node{
     int position;
     int station; //代表站点编号，若为-1表示不是站点
@@ -29,10 +24,11 @@ ROAD * appendNode(ROAD* head, int pos);
 extern ROAD * head;
 extern ROAD * bus;
 
+void config();
+void FCFS();
+void SSTF();
+void SCAN();
 void getorder();//获取指令
-
-//void print();//打印状态
-
 void action();
 
 #endif
