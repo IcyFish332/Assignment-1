fad97f909480e761b4305afef7f7c64af943bade
Siyuan <lusiyuanzs@gmail.com>
2022-05-11 21:55:15 +0800

    For SCAN

--- SCAN.cpp
+++ SCAN.cpp
@@ -1,20 +1,34 @@
 #include"base.h"
 
-void request_for_SCAN();
-void create_for_SCAN();
-void print_for_SCAN();
-int cleared(int total[]);
+void request_for_SCAN(int * total);
+void create_for_SCAN(int *target,int * wise,int * ccwise,int *total);
+void print_for_SCAN(int *target,int * wise,int * ccwise);
+int cleared(int * total);
+int ruler(int flag);
 
 void SCAN()
 {
     door=1;
-    int ccwise[TOTAL_STATION]={0};
-    int wise[TOTAL_STATION]={0};
-    int target[TOTAL_STATION]={0};
-    int total[TOTAL_STATION*DISTANCE]={0};
     int flag;
+    int * ccwise=new int [TOTAL_STATION];
+    int * wise=new int [TOTAL_STATION];
+    int * target=new int [TOTAL_STATION];
+    int * total=new int [TOTAL_STATION*DISTANCE];
+
+    print_for_SCAN(target,wise,ccwise);
     getorder();
-    //确定初始方向
+    while(order=="clock"){
+        print_for_SCAN(target,wise,ccwise);
+        getorder();
+    }
+    while(order!="clock"){
+        create_for_SCAN(target,wise,ccwise,total);
+        getorder();
+    }
+    direction=0;
+    request_for_SCAN(total);
+    action();
+    print_for_SCAN(target,wise,ccwise);
     getorder();
     while(order!="end"){
         if(order=="clock"){
@@ -30,18 +44,80 @@ void SCAN()
                 door=0;
                 if(cleared(total)==1) direction=-1;
             }
-            print_for_SCAN();
+            print_for_SCAN(target,wise,ccwise);
         }
         else{
-            create_for_SCAN();
-            request_for_SCAN();
+            create_for_SCAN(target,wise,ccwise,total);
+            request_for_SCAN(total);
         }
         getorder();
     }
-
+    cout<<"end"<<endl;
+    delete []ccwise;
+    delete []wise;
+    delete []target;
+    delete []total;
 }
 
-int cleared(int total[])
+void request_for_SCAN(int * total)
 {
+    int min=100;
+    for(int i=0;i<TOTAL_STATION*DISTANCE;i++){
+        if(total[i]==1){
+            if(ruler(i)<min) min=ruler(i);
+        }
+    }
+    if(min>TOTAL_STATION*DISTANCE*0.5) direction=(direction+1)%2;
+}
 
+void create_for_SCAN(int *target,int * wise,int * ccwise,int * total)
+{
+    char * temp;
+    char * token;
+    char * number;
+    int num;
+    strcpy(temp,order.c_str());
+    token=strtok(temp," ");
+    number=strtok(NULL," ");
+    num=atoi(number);
+    if(strcmp(token, "target") == 0)  target[num-1]=1;
+    if(strcmp(token, "clockwise") == 0)  wise[num-1]=1;
+    if(strcmp(token, "counterclockwise") == 0)  ccwise[num-1]=1;
+    total[(num-1)*DISTANCE]=1;
+}
+
+void print_for_SCAN(int *target,int * wise,int * ccwise)
+{
+    cout<<"BUS:"<<endl;
+    cout<<"position:"<<bus->position<<endl;
+    cout<<"target: ";
+    for(int i=0;i<TOTAL_STATION;i++)
+        cout<< target[i];
+    cout<<endl<<"STATION:"<<endl<<"clockwise: ";
+    for(int i=0;i<TOTAL_STATION;i++)
+        cout<< wise[i];
+    cout<<endl<<"counterclockwise: ";
+    for(int i=0;i<TOTAL_STATION;i++)
+        cout<< ccwise[i];
+    cout<<endl;
+}
+
+int cleared(int * total)
+{
+    for(int i=0;i< TOTAL_STATION;i++){
+        if(total[i]==1) return 0;
+    }
+    return 1;
+}
+
+int ruler(int flag)
+{
+    if((flag-1)*DISTANCE>bus->position){
+        if(direction==0) return (flag-1)*DISTANCE-bus->position;
+        if(direction==1) return TOTAL_STATION*DISTANCE-(flag-1)*DISTANCE+bus->position;
+    }
+    if((flag-1)*DISTANCE<bus->position){
+        if(direction==0) return bus->position-(flag-1)*DISTANCE;
+        if(direction==1) return TOTAL_STATION*DISTANCE+(flag-1)*DISTANCE-bus->position;
+    }
 }
