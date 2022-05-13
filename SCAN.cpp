2be39dd005cdb2adeba6afb170444d4f270536ff
Siyuan <lusiyuanzs@gmail.com>
2022-05-13 19:33:44 +0800

    For SCAN

--- SCAN.cpp
+++ SCAN.cpp
@@ -3,8 +3,8 @@
 int request_for_SCAN(int * total);
 void create_for_SCAN(int *target,int * wise,int * ccwise,int *total);
 void print_for_SCAN(int *target,int * wise,int * ccwise);
-int cleared(int * total);
 int ruler(int flag);
+int findmin(int * total);
 
 void SCAN()
 {
@@ -14,8 +14,8 @@ void SCAN()
     int * wise=new int [TOTAL_STATION];
     int * target=new int [TOTAL_STATION];
     int * total=new int [TOTAL_STATION*DISTANCE];
-
     bus=creatroutine();
+
     print_for_SCAN(target,wise,ccwise);
     getorder();
     while(order=="clock"){
@@ -26,34 +26,29 @@ void SCAN()
         create_for_SCAN(target,wise,ccwise,total);
         getorder();
     }
-    direction=0;
-    flag=request_for_SCAN(total);
-    cout<< flag <<endl;
+    flag= findmin(total);
     action();
     print_for_SCAN(target,wise,ccwise);
     getorder();
     while(order!="end"){
         if(order=="clock"){
-            if(door==0){
+            if(door==0&&flag!=-1){
                 action();
                 if(flag==bus->position) door=1;
             }
             else{
-                total[flag]=0;
-                ccwise[flag/DISTANCE+1]=0;
-                wise[flag/DISTANCE+1]=0;
-                target[flag/DISTANCE+1]=0;
+                total[bus->position]=0;
+                ccwise[bus->position/DISTANCE]=0;
+                wise[bus->position/DISTANCE]=0;
+                target[bus->position/DISTANCE]=0;
                 door=0;
-                if(cleared(total)==1) direction=-1;
                 flag=request_for_SCAN(total);
-                cout<< flag <<endl;
             }
             print_for_SCAN(target,wise,ccwise);
         }
         else{
             create_for_SCAN(target,wise,ccwise,total);
             flag=request_for_SCAN(total);
-            cout<< flag <<endl;
         }
         getorder();
     }
@@ -71,12 +66,13 @@ int request_for_SCAN(int * total)
     int min=100;
     for(int i=0;i<TOTAL_STATION*DISTANCE;i++){
         if(total[i]==1){
-            if(ruler(i)<min) {
+            if(ruler(i)<min&&ruler(i)>0) {
                 min=ruler(i);
                 flag=i;
             }
         }
     }
+    if(min==100) return -1;
     if(min>TOTAL_STATION*DISTANCE*0.5) direction=(direction+1)%2;
     return flag;
 }
@@ -114,14 +110,6 @@ void print_for_SCAN(int *target,int * wise,int * ccwise)
     cout<<endl;
 }
 
-int cleared(int * total)
-{
-    for(int i=0;i< TOTAL_STATION;i++){
-        if(total[i]==1) return 0;
-    }
-    return 1;
-}
-
 int ruler(int flag)
 {
     if(flag>bus->position){
@@ -131,5 +119,38 @@ int ruler(int flag)
     if(flag<bus->position){
         if(direction==0) return TOTAL_STATION*DISTANCE+flag-bus->position;
         if(direction==1) return bus->position-flag;
+    }
+    if(flag==bus->position) return 0;
+}
+
+int findmin(int * total)
+{
+    int min0,min1,flag0,flag1;
+    min0=min1=100;
+    direction=0;
+    for(int i=0;i<TOTAL_STATION*DISTANCE;i++){
+        if(total[i]==1){
+            if(ruler(i)<min0&&ruler(i)>0){
+                min0=ruler(i);
+                flag0=i;
+            }
+        }
+    }
+    direction=1;
+    for(int i=0;i<TOTAL_STATION*DISTANCE;i++){
+        if(total[i]==1){
+            if(ruler(i)<min1&&ruler(i)>0){
+                min1=ruler(i);
+                flag1=i;
+            }
+        }
+    }
+    if(min0<=min1) {
+        direction=0;
+        return flag0;
+    }
+    else {
+        direction=1;
+        return flag1;
     }
 }
