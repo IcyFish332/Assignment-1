50bd9bf84e1e1fc1e413fd1ed195281ac28d43b9
Siyuan <lusiyuanzs@gmail.com>
2022-06-03 20:58:27 +0800

    Version 1 is Done!

--- SCAN.cpp
+++ SCAN.cpp
@@ -20,68 +20,44 @@ void SCAN()
         total[i]=0;
     bus=creatroutine();
     door=0;
+    flag=-1;
 
     print(target,wise,ccwise);
     getorder();
-    while(order=="clock"){
-        print(target,wise,ccwise);
-        getorder();
-    }
-    if(order!="end"){
-        while(order!="clock"){
-            create(target,wise,ccwise,total);
-            getorder();
-        }
-        flag= (findmin(total)-1)* DISTANCE;
-        action();
-        if(flag==bus->position) door=1;
-        print(target,wise,ccwise);
-        getorder();
-    }
     while(order!="end"){
         if(order=="clock"){
-            if(door==0&&flag>=0){
-                action();
+            if(door==0&&flag!=-1){
+                flag=request_for_SCAN(total);
+                if(flag!=-1) action();
                 if(flag==bus->position) door=1;
-                print(target,wise,ccwise);
             }
-            else{
+            else if(door==1){
                 total[bus->position]=0;
                 ccwise[bus->position/DISTANCE]=0;
                 wise[bus->position/DISTANCE]=0;
                 target[bus->position/DISTANCE]=0;
                 flag=request_for_SCAN(total);
-                print(target,wise,ccwise);
                 door=0;
-                if(flag==-1) {
-                    getorder();
-                    if(order=="end") break;
-                    while(order!="clock"){
-                        create(target,wise,ccwise,total);
-                        getorder();
-                    }
-                    if(order=="end") break;
-                    for(int i=0;i<TOTAL_STATION*DISTANCE;i++){
-                        if(total[i]==1){
-                            if(ruler(i)==0){
-                                total[bus->position]=0;
-                                ccwise[bus->position/DISTANCE]=0;
-                                wise[bus->position/DISTANCE]=0;
-                                target[bus->position/DISTANCE]=0;
-                            }
+            }
+            else if(flag==-1){
+                for(int i=0;i<TOTAL_STATION*DISTANCE;i++){
+                    if(total[i]==1){
+                        if(ruler(i)==0){
+                            total[bus->position]=0;
+                            ccwise[bus->position/DISTANCE]=0;
+                            wise[bus->position/DISTANCE]=0;
+                            target[bus->position/DISTANCE]=0;
                         }
                     }
-                    flag = (findmin(total) - 1) * DISTANCE;
-                    if(flag>=0) action();
-                    if(flag==bus->position) door=1;
-                    print(target,wise,ccwise);
                 }
+                flag = (findmin(total) - 1) * DISTANCE;
+                if(flag<0) flag=-1;
+                else action();
             }
+            print(target,wise,ccwise);
         }
-        else{
+        else
             create(target,wise,ccwise,total);
-            flag=request_for_SCAN(total);
-        }
         getorder();
     }
     cout<<"end"<<endl;
